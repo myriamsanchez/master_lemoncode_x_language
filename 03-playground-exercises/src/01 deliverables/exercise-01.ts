f7aa42a23727ee135aee39d478664d7e45abf1aa
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-06-12 21:20:06 +0200

    first commit, added the code for exercise 1

---
+++ 03-playground-exercises/src/01 deliverables/exercise-01.ts
@@ -0,0 +1,19 @@
+console.log("************** DELIVERABLE 01 *********************");
+const animals: string[] = ["cat", "dog", "duck", "butterfly", "bee", "horse"];
+console.log ("original array: ", animals);
+
+console.log("head function (immutable), that returns first array element. Use destructuring");
+const head = ([head]: string[]) => head;
+console.log(head(animals));
+
+console.log("tail function (immutable), that returns all array elements but first. Use rest operator");
+const tail = ([,...tail]: string[]) => tail;
+console.log(tail(animals));
+
+console.log("init function(immutable), that returns all array elements but last");
+const init = (array: string[]) => array.slice(0, array.length-1);
+console.log(init(animals));
+
+console.log("last function(immutable), that returns last array element");
+const last = (array: string[]) => array.slice(-1);
+console.log(last(animals));
---
+++ 03-playground-exercises/src/01 deliverables/exercise-02.ts
@@ -0,0 +1,3 @@
+console.log("************** DELIVERABLE 02 *********************");
+const people: string[] = ["Paul", "John", "Lucy", "Becky", "Dan"];
+console.log()
