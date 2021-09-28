2fb8c2dd65dfa8e59cae8122f723e5a9028b3770
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-09-29 01:09:03 +0200

    added exercise 2 and 2 optional

--- 03-playground-exercises/src/01 deliverables/exercise-02.ts
+++ 03-playground-exercises/src/01 deliverables/exercise-02.ts
@@ -1,3 +1,16 @@
 console.log("************** DELIVERABLE 02 *********************");
 const people: string[] = ["Paul", "John", "Lucy", "Becky", "Dan"];
-console.log()
+const animals2: string[] = ["cat", "dog", "duck", "butterfly", "bee", "horse"];
+
+console.log("concat function (inmutable) tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos. Utiliza rest / spread operators");
+
+const concat = (a: any[], b: any[]) => [...a, ...b];
+console.log(concat(people, animals2));
+
+console.log("*Opcional: Implementa una versión del ejercicio anterior donde se acepten múltiples arrays de entrada (más de 2)");
+const concatMulti = (...args: any[]) => {
+  const newArray = [];
+  args.forEach((element) => newArray.push(...element));
+  return newArray;
+};
+console.log(concatMulti(people, animals2, people));
