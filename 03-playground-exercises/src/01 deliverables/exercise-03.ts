3e9233805922444427393e5b584eb7ca3d120f5c
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-09-29 01:42:10 +0200

    added exercise 3

--- 03-playground-exercises/src/01 deliverables/exercise-03.ts
+++ 03-playground-exercises/src/01 deliverables/exercise-03.ts
@@ -1,1 +1,20 @@
 console.log("************** DELIVERABLE 03 *********************");
+console.log("Implementa una función `clone` que, a partir de un objeto de entrada `source` devuelva un nuevo objeto con las propiedades de `source`:");
+const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
+const b = { name: "Luisa", age: 31, married: true };
+
+function clone(source: object) {
+  return {...source}
+}
+
+const newObject = clone(a);
+console.log(newObject, (newObject === a));
+
+console.log("Implementa una función `merge` que, dados dos objetos de entrada `source` y `target`, devuelva un nuevo objeto con todas las propiedades de `target` y de `source`, y en caso de propiedades con el mismo nombre, `source` sobreescribe a `target`.");
+
+
+function merge(source: object, target: object) {
+  return {...target, ...source};
+}
+
+console.log(merge(a, b));
