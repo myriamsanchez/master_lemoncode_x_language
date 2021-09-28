974049d41c0c20bea690df84c6ff032a9ef9ce2e
nekoinverse <myriamsanchez@users.noreply.github.com>
2021-09-29 01:41:55 +0200

    added exercise 3 to the list of js to run

--- 03-playground-exercises/src/index.ts
+++ 03-playground-exercises/src/index.ts
@@ -1,7 +1,7 @@
 // import "./00 practice/exercise.js";
 import "./01 deliverables/exercise-01.js";
 import "./01 deliverables/exercise-02.js";
-// import "./01 deliverables/exercise-03.js";
+import "./01 deliverables/exercise-03.js";
 // import "./01 deliverables/exercise-04.js";
 // import "./01 deliverables/exercise-05.js";
 // import "./02 challenges/exercise.js";
